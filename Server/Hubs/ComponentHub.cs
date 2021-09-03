b4d6d3953086fcd9c35cfa2df3e36bf955532f45
Chase Rony <chase.rony@appech.com>
2021-09-02 20:05:23 -0400

    Initial Commit

---
+++ Server/Hubs/ComponentHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Hubs
+{
+    public class ComponentHub : Hub
+    {
+        public async Task SendComponent(string code)
+        {
+            await Clients.All.SendAsync("SendComponent", code);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine($"{Context.ConnectionId} connected");
+            return base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception e)
+        {
+            Console.WriteLine($"Disconnected {e?.Message} {Context.ConnectionId}");
+            await base.OnDisconnectedAsync(e);
+        }
+    }
+}
